6a90e1287319e4f0e5813ea8ac9618d474897c50
Devin Weaver <suki@tritarget.org>
2014-11-10 17:14:42 -0500

    Implement bin/todue command

--- bin/todue.js
+++ bin/todue.js
@@ -1,10 +1,47 @@
 #!/usr/bin/env node
+var fs    = require('fs');
+var path  = require('path');
+var pkg   = require('../package.json');
+var ToDue = require('../lib/todue');
 
-console.log('TODO: CLI incomplete');
-process.exit(1);
+var argv = require('yargs')
+  .usage('Usage: $0 [--config CONFIG | FILE]')
+  .example('$0 -', 'Read STDIN for todo list.')
+  .example('$0 ~/todo/done.txt', 'Use ~/todo/done.txt for list of todos.')
+  .version(pkg.version, 'version')
+  .alias('version', 'v')
+  .help('help')
+  .alias('help', 'h')
+  .argv;
 
-// return todoStream
-//   .pipe(todoParse())
-//   .pipe(filterComplete())
-//   .pipe(filterByDueDate(moment()))
-//   .pipe(sendToProwl('apiKey'));
+function exitSuccess() {
+  process.exit(0);
+}
+
+function exitError(err) {
+  console.error(err);
+  process.exit(2);
+}
+
+function processTodo(stream) {
+  var apiKey = {push: function() {}};
+
+  stream
+  .pipe(ToDue.todoParse())
+  .pipe(ToDue.filterComplete())
+  .pipe(ToDue.filterByDueDate())
+  .pipe(ToDue.sendToProwl(apiKey))
+  .on('finish', exitSuccess)
+  .on('error', exitError);
+}
+
+var todoStream;
+var todoFile = argv._[0] || process.env.TODO_FILE || '-';
+
+if (todoFile === '-') {
+  todoStream = process.stdin;
+} else {
+  todoStream = fs.createReadStream(todoFile);
+}
+
+processTodo(todoStream);
